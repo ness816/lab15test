d7dbf96f1eae220977480cfed38c6cbe07eb6848
ness816 <ness41861@mail.com>
2024-03-06 23:27:18 +0700

    test15

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,60 @@
-include <iostream>
+#include<iostream>
 using namespace std;
 
-int main(){
+int main()
+{
+	int a = 5;
+	string b = "A";
+	string &c = b;
+	int *x = &a;
+	string *y = &b;
+	int **z = &x;
 
+	cout<< a <<" ";
+	cout<< b <<" ";
+	cout<< c <<" ";
+	cout<< x <<" ";
+	cout<< y <<" ";
+	cout<< z <<endl;
+
+	cout<< &a <<" ";
+	cout<< &b <<" ";
+	cout<< &c <<" ";
+	cout<< &x <<" ";
+	cout<< &y <<" ";
+	cout<< &z <<endl;
+
+	c = "F";
+	cout<< a <<" ";
+	cout<< b <<" ";
+	cout<< c <<" ";
+	cout<< x <<" ";
+	cout<< y <<" ";
+	cout<< z <<endl;
+
+	*y = "W";
+	cout<< a <<" ";
+	cout<< b <<" ";
+	cout<< c <<" ";
+	cout<< x <<" ";
+	cout<< y <<" ";
+	cout<< z <<endl;
+
+	*x = 6;
+	cout<< a <<" ";
+	cout<< b <<" ";
+	cout<< c <<" ";
+	cout<< x <<" ";
+	cout<< y <<" ";
+	cout<< z <<endl;
 	
+	**z = 7;
+	cout<< a <<" ";
+	cout<< b <<" ";
+	cout<< c <<" ";
+	cout<< x <<" ";
+	cout<< y <<" ";
+	cout<< z <<endl;
 	
 	return 0;
 }
--- lab15_4.cpp
+++ lab15_4.cpp
@@ -17,3 +17,21 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a,int &b,int &c,int &d)
+{
+	int x[] = {a,b,c,d};
+	for(int i = 0 ; i < 4 ; i++)
+	{
+		int y = rand()%4;
+		int t = x[i];
+		x[i] = x[y];
+		x[y] = t;
+	}
+
+	a = x[0];
+	b = x[1];
+	c = x[2];
+	d = x[3];
+
+}
